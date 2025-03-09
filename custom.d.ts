9c3a793537d10f1a95934efc6a7bb68518017bc3
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-03-09 14:03:05 +0100

    setup website

---
+++ custom.d.ts
@@ -0,0 +1,8 @@
+declare module '*.html' {
+    const content: any;
+    export default content;
+}
+declare module '*.svg' {
+    const content: any;
+    export default content;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world")
+
+const main = document.querySelector("main")
+import "./styles.css"
+
+import home  from "./pages/home/home.html";
+//addHtml(home, undefined); // initial page
+document.querySelector("nav button.home").addEventListener("click",(e)=> addHtml(home,e.target));
+import menu  from "./pages/menu/menu.html";
+document.querySelector("nav button.menu").addEventListener("click",(e)=> addHtml(menu,e.target));
+import reviews  from "./pages/reviews/reviews.html";
+document.querySelector("nav button.reviews").addEventListener("click",(e)=> addHtml(reviews,e.target));
+import contact  from "./pages/contact/contact.html";
+document.querySelector("nav button.contact").addEventListener("click",(e)=> addHtml(contact,e.target));
+
+
+
+function addHtml(html:string, target: EventTarget){
+    document.querySelectorAll("nav button").forEach((button) => button.classList.remove("selected"));
+    if ( target instanceof HTMLElement) target.classList.add("selected");
+    main.innerHTML = '';
+    main.innerHTML = html;
+}
