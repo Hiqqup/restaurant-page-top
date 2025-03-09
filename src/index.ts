677d1deea3f30c2772c73ab9c7305ca2175ee16c
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-03-09 14:07:41 +0100

    update start page

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const main = document.querySelector("main")
 import "./styles.css"
 
 import home  from "./pages/home/home.html";
-//addHtml(home, undefined); // initial page
+addHtml(home, undefined); // initial page
 document.querySelector("nav button.home").addEventListener("click",(e)=> addHtml(home,e.target));
 import menu  from "./pages/menu/menu.html";
 document.querySelector("nav button.menu").addEventListener("click",(e)=> addHtml(menu,e.target));
